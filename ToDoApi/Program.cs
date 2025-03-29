4eda4e51f7f3251c2c9d67f9a3adacbab1df90a1
fquea15 <qjcoders@gmail.com>
2025-03-29 08:44:40 -0500

    Removing unnecessary files to improve project structure

--- ToDoApi/Program.cs
+++ ToDoApi/Program.cs
@@ -11,10 +11,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-  app.UseExceptionHandler("/Home/Error");
-  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-  // app.UseHsts();
-
   app.UseSwagger();
   app.UseSwaggerUI(c =>
   {
@@ -25,15 +21,5 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseRouting();
-
-// app.UseAuthorization();
-
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-  .WithStaticAssets();
-
-
+app.MapControllers();
 app.Run();
